8192ba8ee65730094afcc1bbe0cfdfa6b976aac0
TalNoamX <talnoamb@gmail.com>
2020-07-20 19:21:51 +0300

    start to work on LR

---
+++ LR/greyScale.py
@@ -0,0 +1,18 @@
+####first arg => where to
+####second arg=> from where
+####third arg=> which postfix
+
+
+from PIL import Image
+import glob
+import os
+
+path = "C:\\Users\\user1\PycharmProjects\gender-classification-1\Dataset\grey-Validation\Female"
+folder = "C:\\Users\\user1\PycharmProjects\gender-classification-1\Dataset\Validation\Female" +  r'\*.' + "jpg"
+num = 0
+for filename in glob.glob(folder):
+    im = Image.open(filename).convert('L')
+    im = im.resize((100, 100), Image.ANTIALIAS)
+    n_path = os.path.join(path, str(num)) + '.' + "jpg"
+    im.save(n_path)
+    num += 1
